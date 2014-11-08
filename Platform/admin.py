0833abc608122fdd9fc15fef2ac192a9a88839b4
arpit <arpit87@gmail.com>
2014-11-09 05:26:43 +0530

    dev profile

--- Platform/admin.py
+++ Platform/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+import Platform
 # Register your models here.
+class PlatformModelAdmin(admin.ModelAdmin):
+    list_display = ('bbdid','appuuid')
+
+
+admin.site.register(Platform,PlatformModelAdmin)
