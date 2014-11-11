b76de669d8af77fdc3c5c30c9e95eaa88585715f
arpit <arpit87@gmail.com>
2014-11-12 02:08:15 +0530

    11 nov

--- Beep/models.py
+++ Beep/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Beep(models.Model):
@@ -9,6 +10,7 @@ class Beep(models.Model):
     image_str = models.CharField(max_length=255,null=True)
     rebeeps = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    date_created = models.DateTimeField(null=False,default=datetime.now())
     misc_col1 = models.CharField(max_length=255)
     misc_col2 = models.CharField(max_length=255)
     misc_col3 = models.IntegerField(null=True)
@@ -21,7 +23,7 @@ class SentBeep(models.Model):
     beepid = models.IntegerField(null=False)
     from_id = models.IntegerField(null=False)
     to_id = models.IntegerField(null=False)
-    date_time = models.DateTimeField(null=False)
+    date_time = models.DateTimeField(null=False,default=datetime.now())
     misc_col1 = models.CharField(max_length=255)
     misc_col2 = models.CharField(max_length=255)
 
