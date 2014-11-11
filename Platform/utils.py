0846bd5a678d7aaa07bd7ff6b2cc17fd8186a896
arpit <arpit87@gmail.com>
2014-11-12 03:01:29 +0530

    11 nov

--- Platform/utils.py
+++ Platform/utils.py
@@ -1,11 +1,13 @@
 import json
 
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
 def successJson(jsonObj):
     jsonreturnObj = dict({"Status":"Success" , "body":jsonObj})
-    return json.dumps(jsonreturnObj)
+    return json.dumps(jsonreturnObj,default=date_handler)
 
 
 def errorJson(jsonObj):
     jsonreturnObj = dict({"Status":"Error" , "error":jsonObj})
-    return json.dumps(jsonreturnObj)
+    return json.dumps(jsonreturnObj,default=date_handler)
