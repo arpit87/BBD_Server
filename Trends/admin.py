49ffaa4c544bf6456b6a757a8c061ad79674830f
arpit <arpit87@gmail.com>
2014-11-12 03:14:55 +0530

    11 nov

--- Trends/admin.py
+++ Trends/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from Trends.models import beeptimetrends,individualbeeptrends
 
-# Register your models here.
+class BeepTimeTrendsModelAdmin(admin.ModelAdmin):
+    list_display = ('beep_id','beep_freq','trend_type')
+
+class IndividualTimeTrendsModelAdmin(admin.ModelAdmin):
+    list_display = ('beep_id','beep_freq','trend_type')
+
+
+admin.site.register(beeptimetrends,BeepTimeTrendsModelAdmin)
+admin.site.register(individualbeeptrends,IndividualTimeTrendsModelAdmin)
