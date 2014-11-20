08cf1d87388771f3ca5cee1a047590d75527e195
arpit <arpit87@gmail.com>
2014-11-20 23:52:55 +0530

    trends improved

--- Trends/admin.py
+++ Trends/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from Trends.models import beeptimetrends,individualbeeptrends,TrendsUpdateLastTime
 
 class BeepTimeTrendsModelAdmin(admin.ModelAdmin):
-    list_display = ('beep','beep_freq','trend_type')
+    list_display = ('beep','beep_freq','is_new','trend_type')
 
 class IndividualTimeTrendsModelAdmin(admin.ModelAdmin):
     list_display = ('bbs_id','beep_freq','trend_type')
